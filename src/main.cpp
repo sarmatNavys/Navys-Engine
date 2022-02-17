fc4561e2008c26e954c3a6eeb81dd65fb4687598
Sarmat Vashaev <sarmatvashaev2004@gmail.com>
2022-02-18 00:07:37 +0300

    OpenGL initialize

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,52 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+int g_windowSizeX = 640;
+int g_windowSizeY = 480;
+
+void glfwWindowSizeCallback(GLFWwindow* window, int width, int height)
+{
+    g_windowSizeX = width;
+    g_windowSizeY = height;
+
+    glViewport(0, 0, width, height);
+}
+
+void glfwKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
+
 int main(void)
 {
     GLFWwindow* window;
 
     /* Initialize the library */
     if (!glfwInit())
+    {
+        std::cout << "Can't initialize glfwInit" << std::endl;
         return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Navys Engine", NULL, NULL);
+    window = glfwCreateWindow(g_windowSizeX, g_windowSizeY, "Navys Engine", nullptr, nullptr);
     if (!window)
     {
+        std::cout << "Can't create window" << std::endl;
+
         glfwTerminate();
         return -1;
     }
+
+    glfwSetWindowSizeCallback(window, glfwWindowSizeCallback);
+    glfwSetKeyCallback(window, glfwKeyCallback);
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
@@ -27,7 +58,8 @@ int main(void)
         return -1;
     }
 
-    std::cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << std::endl;
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 
     glClearColor(0, 1, 0, 1);
 
